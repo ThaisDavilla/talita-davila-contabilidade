1866b9850dd43e9c123880d3ffc0387705ed185b
Thais D'Avilla <thaisdavilla@yahoo.com.br>
2024-06-27 16:41:35 -0300

    Correções da responsividade. Validação do formulário com Javascript.

---
+++ styles/script.js
@@ -0,0 +1,15 @@
+document
+  .getElementById("botaoEnviar")
+  .addEventListener("click", validaFormulario);
+
+function validaFormulario() {
+  if (
+    document.getElementById("nome").value != "" &&
+    document.getElementById("email").value != "" &&
+    document.getElementById("phone").value != ""
+  ) {
+    alert("Pronto! Em breve você receberá nosso contato.");
+  } else {
+    alert("Por favor, preencha os campos nome e email.");
+  }
+}
